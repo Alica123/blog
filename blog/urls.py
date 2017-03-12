f5f77f6eb76f3822ab93eb83da49751eeb900cce
Alica <alica@gymmt.sk>
2017-03-12 16:03:12 +0100

    tlacitka iny ako admin nevido

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 
 ]
 # pre danu url yavola metodu post_list ktora vrati html
