879b342aaf843059cab1e6d5e29a55f149460349
Alica <alica@gymmt.sk>
2017-03-12 15:08:35 +0100

    rozsirenie sablony

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^$', views.post_list, name='post_list'),
+
 ]
-# pre danu url yavola metodu post_list ktora vrati html 
+# pre danu url yavola metodu post_list ktora vrati html
--- mysite/urls.py
+++ mysite/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'', include('blog.urls')),
+    #zoznam url, zalezi na poradi
+    #1.pozrie ci zacina url retazcom admin
+    #2 vsetko ostatn
+
 ]
